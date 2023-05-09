27aa50f31f701170ba40cb46aaf1f691ded30dd5
Jacob Spizziri <jspizziri@weare5stones.com>
2023-05-09 05:03:03 -0400

    create repro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,17 @@
+import { useState, useEffect } from 'react';
+import Dita from '@d-i-t-a/reader';
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  const [stuff, setStuff] = useState<any>([]);
+
+  useEffect(() => {
+    // add some sort of bogus usage of the @d-i-t-a/reader package to
+    // force inclusion
+    setStuff(Object.keys(Dita));
+  }, []);
+
+  return (
+    <div>{JSON.stringify(stuff)}</div>
+  );
 }
